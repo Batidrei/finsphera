c05ac46a6686dd98b1e74447a1cb253529ab5d52
Alex Andrei <bastidaflores@gmail.com>
2025-12-23 23:17:15 -0600

    Change color button

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -7,13 +7,13 @@ export default function ThemeToggle() {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => setMounted(true), []);
-  
-  if (!mounted) return <div className="p-9" />; 
+
+  if (!mounted) return <div className="p-9" />;
 
   return (
     <button
       onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-      className="flex items-center gap-2 p-2 rounded-lg bg-gray-200 dark:bg-zinc-800 text-gray-800 dark:text-zinc-200 transition-all active:scale-95 hover:ring-2 ring-blue-500 cursor-pointer"
+      className="flex items-center gap-2 p-2 rounded-lg bg-gray-200 dark:bg-slate-800 text-gray-800 dark:text-zinc-200 transition-all active:scale-95 hover:ring-2 ring-blue-500 cursor-pointer"
     >
       {resolvedTheme === "dark" ? (
         <><span>☀️</span></>
