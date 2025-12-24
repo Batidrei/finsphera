f567a13b71972a736ab83525808abd67176cd08e
Alex Andrei <bastidaflores@gmail.com>
2025-12-23 23:16:55 -0600

    Change background color

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 import LaunchesClient from "./api/spacex/launches/LaunchesClient";
-import ThemeToggle from "./components/ThemeToggle";
 
 export default function LaunchesPage() {
   return (
-    <div className="min-h-screen transition-colors duration-300 dark:bg-zinc-800">
-      <div className="flex items-center p-8 shadow-md dark:bg-zinc-900 dark:border-zinc-800">
+    <div className="min-h-screen transition-colors duration-300 bg-gray-200 dark:bg-zinc-800">
+      <div className="flex items-center p-8 shadow-md dark:bg-zinc-900 dark:border-zinc-800 bg-white fixed w-full">
         <h1 className="text-3xl font-bold text-gray-950 dark:text-white mr-1">
           Last launches
         </h1>
-        <ThemeToggle></ThemeToggle>
       </div>
       <LaunchesClient />
     </div>
