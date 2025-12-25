c4264a19678b39dafb7afed74388ae684db263d6
Alex Andrei <bastidaflores@gmail.com>
2025-12-25 16:03:24 -0600

    Delete margin in text heading

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function LaunchesPage() {
   return (
     <div className="min-h-screen transition-colors duration-300 bg-gray-200 dark:bg-zinc-800">
       <div className="flex items-center p-8 shadow-md dark:bg-zinc-900 dark:border-zinc-800 bg-white fixed w-full">
-        <h1 className="text-3xl font-bold text-gray-950 dark:text-white mr-1">
+        <h1 className="text-3xl font-bold text-gray-950 dark:text-white">
           Last launches
         </h1>
       </div>
